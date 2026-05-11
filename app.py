064a51e45e5754681500616c186aece4738087c8
Shaik Afreen <sameenaghouse77@gmail.com>
2026-05-11 10:58:17 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask ,render_template , request , jsonify
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Book(db.Model) :
+        id =  db.Column(db.Integer, primary_key=True)
+        book = db.Column(db.String(100), nullable=False)
+        author = db.Column(db.String(100), nullable=False)
+        price = db.Column(db.Float, nullable=False)
+        
+        def to_dict(self) :
+                return {
+                        "id" : self.id,
+                        "book" : self.book,
+                        "author" : self.author,
+                        "price" : self.price
+                }
+
+with app.app_context() :
+        db.create_all()
+
+
+        
+ 
+@app.route('/home' )
+def home() :
+        return render_template("homepage.html" )
+
+@app.route('/about' )
+def about() :
+        return render_template("aboutus.html" )
+
+@app.route('/contact' )
+def contact() :
+        return render_template("contact.html" )
+
+# API Endpoints
+
+@app.route('/api/books/<int:id>')
+def get_single_book(id):
+        book=Book.query.get(id) #got data in object format
+        book_dict=book.to_dict()  #object-->dict
+        book_json=jsonify(book_dict) #Dict-->json
+
+        return book_json
+          #data sent through api
+        
+
+
+
+@app.route('/api/books', methods=['POST',])
+def add_book():
+
+    data = request.get_json()
+
+    new_book = Book(
+        book=data['book'],
+        author=data['author'],
+        price=data['price']
+    )
+
+    db.session.add(new_book)
+    db.session.commit()
+
+    return jsonify(new_book.to_dict()), 201
+        
+@app.route('/api/books/<int:id>', methods=['PATCH',])
+def update_book(id):
+        data_to_update=request.get_json()  #getting from api
+        old_data=Book.query.get(id)   #got data in object
+        old_data.book=old_data.get('book',old_data.book)
+
+
+if __name__ == "__main__" :                                                                                                           
+        app.run(debug=True)
